c7613ee4cd0dcfd93be625e283e962463c2483a2
Petar Rankovic <theroppex@gmail.com>
2018-01-01 13:00:02 +0100

    adds navbar and login html

--- frontend/festivali/src/app/app.module.ts
+++ frontend/festivali/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
